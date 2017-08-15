8c1c8d3a33cf0bf415dd5fc12db234256090c539
Mwangi <kidynamit@users.noreply.github.com>
2017-08-15 19:55:08 +0200

    functions to reimplement

--- include/vtkFitsReader.h
+++ include/vtkFitsReader.h
@@ -7,34 +7,46 @@
 #ifndef __vtkFitsReader_h
 #define __vtkFitsReader_h
 
-#include "vtkStructuredPointsSource.h"
-extern "C" {
-  #include "fitsio.h"
-}
+#include <vtkDataReader.h>
+#include <vtkStructuredPointsReader.h>
+#include <fitsio.h>
+#include "vtkIOFitsModule.h"
 
-class VTK_EXPORT vtkFitsReader : public vtkStructuredPointsSource
+class VTK_EXPORT vtkFitsReader : public vtkStructuredPointsReader
 {
-  public:
-  vtkFitsReader();
+public:
 
-  static vtkFitsReader *New() {return new vtkFitsReader;};
-  const char *GetClassName() {return "vtkFitsReader";}
-  void PrintSelf(ostream& os, vtkIndent indent);
+	static vtkFitsReader *New() {return new vtkFitsReader;};
+	vtkTypeMacro(vtkFitsReader, vtkStructuredPointsReader)
+	void PrintSelf(ostream& os, vtkIndent indent);
 
-  void SetFileName(char *name);
-  char *GetFileName(){return filename;}
+	void SetFileName(char *name);
+	char *GetFileName(){return filename;}
 
-  char title[80];
-  char xStr[80];
-  char yStr[80];
-  char zStr[80];
 
-  protected:
-  void Execute();
-  char filename[256]; // static buffer for filename
+protected:
+	vtkFitsReader();
+	~vtkFitsReader();
+	void Execute();
+	void ReadHeader();
+	void printerror(int status); // from fitsio distribution
+	int RequestData(vtkInformation *, vtkInformationVector **,
+		vtkInformationVector *) VTK_OVERRIDE;
 
-  void ReadHeader();
-  void printerror(int status); // from fitsio distribution
+	// Default method performs Update to get information.  Not all the old
+	// structured points sources compute information
+	int RequestInformation(vtkInformation *, vtkInformationVector **,
+		vtkInformationVector *) VTK_OVERRIDE;
+
+	int FillOutputPortInformation(int, vtkInformation *) VTK_OVERRIDE;
+
+private:
+	char title[80];
+	char filename[256]; // static buffer for filename
+	char xStr[80];
+	char yStr[80];
+	char zStr[80];
+
 };
 
 #endif 
